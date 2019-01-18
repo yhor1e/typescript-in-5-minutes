54919c50ada30e70d0ee5f3f4e3dfda75ab9f01c
yhorie <yuk.horie@gmail.com>
2019-01-19 02:04:14 +0900

    4

--- src/greeter.ts
+++ src/greeter.ts
@@ -1,13 +1,22 @@
+class Student {
+    fullName: string;
+    constructor(public firstName: string, public middleInitial: string, public lastName: string) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+
 interface Person {
     firstName: string;
+    middleInitial: string;
     lastName: string;
 }
 
 function greeter(person: Person) {
-    return "Hello, " + person.firstName + person.lastName;
+    return "Hello, " + person.firstName + person.middleInitial + person.lastName;
 }
 
-let user = { firstName: "Jane", lastName: "User" };
+let user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
 
